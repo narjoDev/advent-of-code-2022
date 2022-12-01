aaffb4b808a3384f10c8062b90821f83c1602c63
narjoDev <narjoDev@users.noreply.github.com>
2022-11-30 22:09:16 -0800

    Complete day 1 part 2

--- day-01/solution.js
+++ day-01/solution.js
@@ -37,11 +37,28 @@ function maxListSum(l) {
   return sums.reduce((m, x) => (m > x ? m : x));
 }
 
+function listsToSums(lists) {
+  return lists.map((l) => l.map((j) => parseInt(j)).reduce((a, b) => a + b));
+}
+
+function sumOfTopN(arr, n) {
+  const sortedDescending = [...arr].sort((a, b) => b - a);
+  const topN = sortedDescending.slice(0, n);
+  return topN.reduce((a, b) => a + b);
+}
+
 function partOne(file) {
   return maxListSum(separateLists(file));
 }
 
+function partTwo(file) {
+  return sumOfTopN(listsToSums(separateLists(file)), 3);
+}
+
 console.log(partOne(FILES.example));
 console.log(partOne(FILES.actual));
+
+console.log(partTwo(FILES.example));
+console.log(partTwo(FILES.actual));
 
 module.exports = { FILES };
