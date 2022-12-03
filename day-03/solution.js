d832e5b6c7965f590f6b2f8766c5bf8d437a7e2d
narjoDev <narjoDev@users.noreply.github.com>
2022-12-02 22:34:37 -0800

    Complete day 3 part 2

--- day-03/solution.js
+++ day-03/solution.js
@@ -13,12 +13,8 @@ const FILES = {
   actual: importFile("./day-03/input-actual.txt"),
 };
 
-// console.log(FILES.example);
-// console.log(FILES.actual);
-
 function identifySharedItem(rucksack) {
   const firstHalf = rucksack.slice(0, rucksack.length / 2);
-  // const secondCompartment = rucksack.slice(rucksack.length / 2);
   for (let i = rucksack.length / 2; i < rucksack.length; i++) {
     if (firstHalf.includes(rucksack[i])) return rucksack[i];
   }
@@ -26,15 +22,9 @@ function identifySharedItem(rucksack) {
 
 function getPriority(itemType) {
   const charCode = itemType.charCodeAt(0);
-  // console.log(charCode);
   if (97 <= charCode && charCode <= 122) return charCode - 96;
   if (65 <= charCode && charCode <= 90) return charCode - 38;
 }
-
-// console.log(getPriority("a"));
-// console.log(getPriority("z"));
-// console.log(getPriority("A"));
-// console.log(getPriority("Z"));
 
 function partOne(file) {
   return file
@@ -42,12 +32,33 @@ function partOne(file) {
     .reduce((a, b) => a + b);
 }
 
-function partTwo(file) {}
+function partTwo(file) {
+  const input = [...file];
+  let elf;
+  let group = [];
+  let badges = [];
+  while (typeof (elf = input.pop()) !== "undefined") {
+    // console.log(elf);
+    if (group.length < 3) {
+      group.push(new Set(elf.split("")));
+    }
+    if (group.length === 3) {
+      let [a, b, c] = group;
+      const intersection = new Set([...a].filter((x) => b.has(x) && c.has(x)));
+      // console.log(intersection);
+      const badge = Array.from(intersection)[0];
+      badges.push(badge);
+      group = [];
+    }
+  }
 
-console.log(partOne(FILES.example)); // 157
-console.log(partOne(FILES.actual)); // 7763
+  return badges.map((b) => getPriority(b)).reduce((a, b) => a + b);
+}
 
-// console.log(partTwo(FILES.example));
-// console.log(partTwo(FILES.actual));
+// console.log(partOne(FILES.example)); // 157
+// console.log(partOne(FILES.actual)); // 7763
+
+console.log(partTwo(FILES.example)); // 70
+console.log(partTwo(FILES.actual)); // 2569
 
 module.exports = { FILES };
